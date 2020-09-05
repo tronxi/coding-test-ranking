e1ee49580f734c16049f122163e17bd01115448a
tronxi <sergio.gsanchez97@gmail.com>
2020-09-05 22:13:41 +0200

    added test for factory

---
+++ src/test/java/com/idealista/application/service/scorer/completeadcalculator/CompleteAdCalculatorFactoryTest.java
@@ -0,0 +1,29 @@
+package com.idealista.application.service.scorer.completeadcalculator;
+
+import com.idealista.application.model.Typology;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CompleteAdCalculatorFactoryTest {
+
+    private final CompleteAdCalculatorFactory completeAdCalculatorFactory
+            = new CompleteAdCalculatorFactory();
+
+    @Test
+    public void shouldGetDefaultCalculator() {
+        //GIVEN
+        CompleteAdCalculator completeAdChaletCalculator = new CompleteAdChaletCalculator();
+        CompleteAdCalculator completeAdDefaultCalculator = new CompleteAdDefaultCalculator();
+
+        completeAdCalculatorFactory.register(Typology.DEFAULT, completeAdChaletCalculator);
+        completeAdCalculatorFactory.register(Typology.DEFAULT, completeAdDefaultCalculator);
+        //WHEN
+        CompleteAdCalculator completeAdCalculator =
+                completeAdCalculatorFactory.getCalculator(Typology.DEFAULT);
+
+        //THEN
+        assertTrue(completeAdCalculator instanceof CompleteAdDefaultCalculator);
+    }
+
+}
---
+++ src/test/java/com/idealista/application/service/scorer/sizedescriptioncalculator/SizeDescriptionCalculatorFactoryTest.java
@@ -0,0 +1,29 @@
+package com.idealista.application.service.scorer.sizedescriptioncalculator;
+
+import com.idealista.application.model.Typology;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SizeDescriptionCalculatorFactoryTest {
+
+    private final SizeDescriptionCalculatorFactory sizeDescriptionCalculatorFactory
+            = new SizeDescriptionCalculatorFactory();
+
+    @Test
+    public void shouldGetDefaultCalculator() {
+        //GIVEN
+        SizeDescriptionCalculator sizeDescriptionChaletCalculator = new SizeDescriptionChaletCalculator();
+        SizeDescriptionCalculator sizeDescriptionDefaultCalculator = new SizeDescriptionDefaultCalculator();
+
+        sizeDescriptionCalculatorFactory.register(Typology.DEFAULT, sizeDescriptionChaletCalculator);
+        sizeDescriptionCalculatorFactory.register(Typology.DEFAULT, sizeDescriptionDefaultCalculator);
+        //WHEN
+        SizeDescriptionCalculator sizeDescriptionCalculator =
+                sizeDescriptionCalculatorFactory.getCalculator(Typology.DEFAULT);
+
+        //THEN
+        assertTrue(sizeDescriptionCalculator instanceof SizeDescriptionDefaultCalculator);
+    }
+
+}
