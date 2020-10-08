024772e13c181264416d90590ec05e8cb47270ef
tronxi <sergio.gsanchez97@gmail.com>
2020-10-08 02:43:21 +0200

    added integration test'

--- src/test-it/java/com/idealista/infrastructure/api/controller/AdsControllerIT.java
+++ src/test-it/java/com/idealista/infrastructure/api/controller/AdsControllerIT.java
@@ -13,7 +13,9 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -46,23 +48,6 @@ public class AdsControllerIT {
 
     @Test
     @Sql(scripts = "/database/clean.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(scripts = "/database/unscoredAds.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    public void shouldReturnEmptyQualityAdsIT() {
-        this.webTestClient
-                .get()
-                .uri("ads/quality")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBodyList(QualityAd.class)
-                .value(this::assertEmptyQualityAds);
-    }
-
-    private void assertEmptyQualityAds(List<QualityAd> qualityAdList) {
-        assertEquals(0, qualityAdList.size());
-    }
-
-    @Test
-    @Sql(scripts = "/database/clean.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/database/ads.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     public void shouldReturnPublicAdsIT() {
         this.webTestClient
@@ -76,5 +61,54 @@ public class AdsControllerIT {
 
     private void assertPublicAds(List<PublicAd> publicAdList) {
         assertEquals(2, publicAdList.size());
+    }
+
+    @Test
+    @Sql(scripts = "/database/clean.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/database/unscoredAds.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void shouldCalculateAdsIT() {
+        this.webTestClient
+                .put()
+                .uri("ads/score")
+                .exchange()
+                .expectStatus().isOk();
+
+        this.webTestClient
+                .get()
+                .uri("ads/quality")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(QualityAd.class)
+                .value(this::assertQualityAdsCalculated);
+
+        this.webTestClient
+                .get()
+                .uri("ads/public")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(PublicAd.class)
+                .value(this::assertPublicAdsCalculated);
+    }
+
+    private void assertQualityAdsCalculated(List<QualityAd> qualityAdList) {
+        List<Integer> expectedOrderedIds = Arrays.asList(1, 3, 7, 8);
+
+        List<Integer> actualOrderedIds = qualityAdList.stream()
+                .map(QualityAd::getId)
+                .collect(Collectors.toList());
+
+        assertEquals(4, qualityAdList.size());
+        assertEquals(expectedOrderedIds, actualOrderedIds);
+    }
+
+    private void assertPublicAdsCalculated(List<PublicAd> publicAdList) {
+        List<Integer> expectedOrderedIds = Arrays.asList(9, 2, 4, 5, 6);
+
+        List<Integer> actualOrderedIds = publicAdList.stream()
+                .map(PublicAd::getId)
+                .collect(Collectors.toList());
+
+        assertEquals(5, publicAdList.size());
+        assertEquals(expectedOrderedIds, actualOrderedIds);
     }
 }
